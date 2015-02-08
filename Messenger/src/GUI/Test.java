1ea7cc1021099e277b916af26710ae9c23c71688
Ulquiorrae <hariskrkalic@gmail.com>
2015-02-08 14:52:42 +0100

    dada

---
+++ Messenger/src/GUI/Test.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+	
+		Date d = new Date();
+		System.out.println(d.toString().replaceAll(" ", "_"));
+	}
+}
